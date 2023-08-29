f4cc19fcdb585e58451110541f18b0e83cf6a3b1
Siddharth Mishra <siddharth.mishra7403@gmail.com>
2023-08-29 14:25:01 +0530

    changes 1

--- lab3.py
+++ lab3.py
@@ -1,1 +1,10 @@
-n=input
+n=input("Input a number to be checked")
+count=0
+for i in range(1,n+1):
+    if(n%i==0):
+        count+=1
+if(count==2):
+    print("Prime Number")
+else:
+    print("Not Prime Number")
+#changes in branch 2
